5695462f4ca372fe86b73332d40023da064fb04f
Jose Damian Sanchez <joseda@amdocs.com>
2020-07-17 11:03:51 -0500

    redux react

--- ppmtoo-react-client/src/components/Dashboard.js
+++ ppmtoo-react-client/src/components/Dashboard.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react'
 import ProjectItem from './Project/ProjectItem'
+import CreateProjectButton from './Project/CreateProjectButton';
 //import "bootstrap/dist/css/bootstrap.min.css";
 class Dashboard extends Component {
     render() {
         return (
-            <div>
-                <h1 className="alert alert-warning">Welcome to thae Dashboard</h1>
-                <ProjectItem />
+            <div className="projects">
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12">
+                    <h1 className="display-4 text-center">Projects</h1>
+                    <br />
+                    <CreateProjectButton />
+                    <br />
+                    <hr />
+                    <ProjectItem />
+
+                </div>
             </div>
+        </div>
+    </div>
         )
     }
 }
--- ppmtool/ppmtool/src/main/java/com/damianIntelligence/ppmtool/web/ProjectController.java
+++ ppmtool/ppmtool/src/main/java/com/damianIntelligence/ppmtool/web/ProjectController.java
@@ -13,6 +13,7 @@ import org.springframework.http.StreamingHttpOutputMessage.Body;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,6 +28,7 @@ import com.damianIntelligence.ppmtool.services.ProjectService;
 
 @RestController
 @RequestMapping("/api/project")
+@CrossOrigin
 public class ProjectController {
 
 	@Autowired
